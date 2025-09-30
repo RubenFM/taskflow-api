461f4a94e8086b86d73af0e5442c60c09638e0cd
Ruben <rubenfernandezmolina13@gmail.com>
2025-09-30 17:50:16 +0200

    initial Spring Boot project setup with H2 configuration

---
+++ src/test/java/com/atresmedia/taskflow/TaskflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atresmedia.taskflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskflowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
